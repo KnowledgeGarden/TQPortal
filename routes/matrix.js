0eb9ee8058814fb4c1d7ca63a38b0b172ae1615c
Jack Park <jackpark@gmail.com>
2014-06-23 03:19:44 -0700

    Removed unused files

--- routes/matrix.js
+++
@@ -1,7 +0,0 @@
-/**
- * Matrix router
- */
-exports.matrix = function(req, res){
-	//TODO alternate layout
-  res.render('matrix', { title: 'TQPortal' });
-};
--- routes/orbit.js
+++
@@ -1,7 +0,0 @@
-/**
- * orbit route
- */
-exports.orbit = function(req, res){
-	res.render('orbit', { title: 'Home' });
-
-};
