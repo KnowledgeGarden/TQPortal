275b2fe8d64f76f244e40132f9c767ec014a0e5d
Jack Park <jackpark@gmail.com>
2014-08-20 21:44:50 -0700

    Made geomap coordinates and zoom programmable, cleaned up handlebars

--- apps/geomap.js
+++ apps/geomap.js
@@ -6,10 +6,17 @@ exports.plugin = function(app, environment, ppt, isPrivatePortal) {
 	// Menu
 	/////////////////
 	environment.addApplicationToMenu("/geomap","GeoMap");
-
+    /////////////////
+	// router
+	/////////////////
 	app.get("/geomap", function(req,res) {
 		var data = environment.getCoreUIData(req);
+		//NOTE, we can create a GeoMap model to control map initialization;
+		// here we hard wire some values
+		data.x = "30.0";
+		data.y = "5.0";
+		data.z = "2"; //zoom smaller is further out
 		res.render("geomap",data);
 	});
 
-}
+};
