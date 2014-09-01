2b18489268d4d89a977702a078d4e1d9bc0ecc55
Jack Park <jackpark@gmail.com>
2014-09-01 16:55:01 -0700

    first git

---
+++ apps/rpg/rpginfoboxmodel.js
@@ -0,0 +1,67 @@
+/**
+ * InfoBoxModel
+ * This model should have the ability to read an InfoBox schema (JSON) and
+ * paint an HTML form from it, then handle the InfoBox data.
+ * A SubjectProxy has an infoBox API:
+ *   putInfoBox = function(name, jsonString)
+ *   removeInfoBox = function(name)
+ *   getInfoBoxes = function()
+ *   getInfoBox = function(name)
+ * based on named boxes.
+ */
+var types = require('../../node_modules/tqtopicmap/lib/types')
+	, icons = require('../../node_modules/tqtopicmap/lib/icons')
+	, properties = require('../../node_modules/tqtopicmap/lib/properties')
+	, constants = require('../../core/constants')
+;
+
+var RPGInfoBoxModel = module.exports = function(environment) {
+	var NODE_VALUE_MATRIX = "NodeValueMatrix",
+		USER_VALUE_MATRIX = "UserValueMatrix",
+		GUILD_VALUE_MATRIX = "GuildValueMatrix";
+	var myEnvironment = environment;
+	var topicMapEnvironment = environment.getTopicMapEnvironment();
+	var DataProvider = topicMapEnvironment.getDataProvider();
+	var self = this;
+	
+	//////////////////////////////////////
+	//General getters and setters on a given node
+	// Caller required to persist any changes
+	// Caller's option on what content is set inside each valuematrix
+	//////////////////////////////////////
+	/**
+	 * @param node
+	 * @return can return undefined
+	 */
+	self.getNodeValueMatrix = function(node) {
+		return node.getInfoBox(NODE_VALUE_MATRIX);
+	},
+	
+	self.putNodeValueMatrix = function(node, json) {
+		node.putInfoBox(NODE_VALUE_MATRIX, json);
+	},
+	
+	/**
+	 * @param node
+	 * @return can return undefined
+	 */
+	self.getUserValueMatrix = function(node) {
+		return node.getInfoBox(USER_VALUE_MATRIX);
+	},
+	
+	self.putUserValueMatrix = function(node, json) {
+		node.putInfoBox(USER_VALUE_MATRIX, json);
+	},
+	
+	/**
+	 * @param node
+	 * @return can return undefined
+	 */
+	self.getGuildValueMatrix = function(node) {
+		return node.getInfoBox(GUILD_VALUE_MATRIX);
+	},
+	
+	self.putGuildValueMatrix = function(node, json) {
+		node.putInfoBox(GUILD_VALUE_MATRIX, json);
+	};
+};
