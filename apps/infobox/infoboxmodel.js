0369d5840c5707826a7d3eea46a8866c43495a6d
Jack Park <jackpark@gmail.com>
2014-09-01 16:55:41 -0700

    Adding game infrastructure

--- apps/infobox/infoboxmodel.js
+++ apps/infobox/infoboxmodel.js
@@ -1,15 +1,23 @@
 /**
  * InfoBoxModel
  * This model should have the ability to read an InfoBox schema (JSON) and
- * paint an HTML form from it, then handle the InfoBox data
+ * paint an HTML form from it, then handle the InfoBox data.
+ * A SubjectProxy has an infoBox API:
+ *   putInfoBox = function(name, jsonString)
+ *   removeInfoBox = function(name)
+ *   getInfoBoxes = function()
+ *   getInfoBox = function(name)
+ * based on named boxes.
  */
 var types = require('../../node_modules/tqtopicmap/lib/types')
-, icons = require('../../node_modules/tqtopicmap/lib/icons')
-, properties = require('../../node_modules/tqtopicmap/lib/properties')
-
-  , constants = require('../../core/constants')
+	, icons = require('../../node_modules/tqtopicmap/lib/icons')
+	, properties = require('../../node_modules/tqtopicmap/lib/properties')
+	, constants = require('../../core/constants')
 ;
 
-var InfoBoxModel = function(environment) {
-	
+var InfoBoxModel = module.exports = function(environment) {
+	var myEnvironment = environment;
+	var topicMapEnvironment = environment.getTopicMapEnvironment();
+	var DataProvider = topicMapEnvironment.getDataProvider();
+//TODO
 };
--- apps/rpg/rpgenvironment.js
+++ apps/rpg/rpgenvironment.js
@@ -4,13 +4,19 @@
 var  types = require('../../node_modules/tqtopicmap/lib/types')
 	, sb = require('../../node_modules/tqtopicmap/lib/util/stringbuilder')
 	, constants = require('../../core/constants')
+	, ib = require('./rpginfoboxmodel')
 ;
 
 var RPGEnvironment = module.exports = function(environment, tmenv) {
 	var myEnvironment = environment;
 	var topicMapEnvironment = tmenv;
 	var DataProvider = topicMapEnvironment.getDataProvider();
+	var RPGInfoBoxModel = new ib(environment);
 	console.log("RPGEnvironment started");
 	var self = this;
 	//TODO
+	
+	self.getInfoBoxModel = function() {
+		return RPGInfoBoxModel;
+	}
 };
