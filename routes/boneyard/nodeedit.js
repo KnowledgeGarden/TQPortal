292d4169d78225abb59bb854efc0831db9c15fa3
Jack Park <jackpark@gmail.com>
2015-02-04 09:17:34 -0800

    moved to boneyard

--- routes/boneyard/nodeedit.js
+++ routes/boneyard/nodeedit.js
@@ -1,3 +1,8 @@
+/**
+ * This was a prototype for editing nodes.
+ * It has been replaced by admin.js functions: choosenode and updatenode
+ * This will not compile
+ */
 var kwb = require('../apps/kwb/kwbmodel'),
     relationlist = require('../apps/kwb/relationlist'),
     constants = require('../core/constants'),
@@ -20,12 +25,11 @@ exports.plugin = function(app, environment, ppt, isPrivatePortal) {
 			for(var i=0;i<creds.length;i++) {
 				console.log("Admin.isAdmin-1 "+creds[i]+" "+constants.ADMIN_CREDENTIALS);
 				if (creds[i].trim() === constants.ADMIN_CREDENTIALS) {
-					next();
-					return;
+					 return next();
 				}
 			}
 		}
-		res.redirect('/');
+		return res.redirect('/');
 	}
 
  	function isLoggedIn(req, res, next) {
@@ -37,33 +41,34 @@ exports.plugin = function(app, environment, ppt, isPrivatePortal) {
 		if (isPrivatePortal) {
 			return res.redirect('/login');
 		}
-		res.redirect('/');
+		return res.redirect('/');
 	}
    
-  app.get('/nodeedit/:id', isAdmin,function(req, res) {
-	var q = req.params.id,
-        credentials = req.user.credentials;
-    
-	var data = myEnvironment.getCoreUIData(req);
-	//myEnvironment.logDebug("BLOGGY "+JSON.stringify(req.query));
-      // fetch the node and send its source out to nodeeditform
-	Dataprovider.getNodeByLocator(q, credentials, function(err,result) {
-        data.source = result.toJSON();
-        return res.render('nodeeditform',data);
+	app.get('/nodeedit/:id', isAdmin, function nodeEditGet(req, res) {
+		var q = req.params.id,
+			credentials = req.user.credentials,
+    		data = myEnvironment.getCoreUIData(req);
+		// fetch the node and send its source out to nodeeditform
+		Dataprovider.getNodeByLocator(q, credentials, function nodeEditGetNode(err, result) {
+			if (result) {
+		        data.source = result.toJSON();
+		        return res.render('nodeeditform', data);
+		    } else {
+		    	return res.redirect('/error/NodeEditCannotDisplay');
+		    }
+		});
 	});
-  });
     
-  app.post('/nodeedit', isAdmin, function(req,res) {
-    var body = req.body;
-    var usx = req.user;
-      //TODO turn json into SubjectProxy, set last edit date, and save it.
-    console.log('BLOG_NEW_POST '+JSON.stringify(usx)+' | '+JSON.stringify(body));
-    _blogsupport(body, usx, function(err,result) {
-      console.log('BLOG_NEW_POST-1 '+err+' '+result);
-      //technically, this should return to "/" since Lucene is not ready to display
-      // the new post; you have to refresh the page in any case
-      return res.redirect('/blog');
-    });
-  });
+	app.post('/nodeedit', isAdmin, function nodeEditPost(req, res) {
+		var body = req.body,
+			usx = req.user;
+		//TODO turn json into SubjectProxy, set last edit date, and save it.
+		console.log('BLOG_NEW_POST '+JSON.stringify(usx)+' | '+JSON.stringify(body));
+		//WARNING: this will not compile -- missing _blogsupport from blog.js
+			_blogsupport(body, usx, function nodeEditSupport(err, result) {
+			console.log('BLOG_NEW_POST-1 '+err+' '+result);
+			return res.redirect('/);
+		});
+	});
 
 };
